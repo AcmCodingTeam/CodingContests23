236513b80397406e141eb289754b91b83fbaf4dc
karthik224-hash <68278634+karthik224-hash@users.noreply.github.com>
2021-01-10 16:33:38 +0530

    Update uberpool.cpp

--- Contest 2/uberpool.cpp
+++ Contest 2/uberpool.cpp
@@ -1,3 +1,32 @@
+/*
+link:https://www.hackerrank.com/contests/junior-challenge2/challenges/uber-pool
+    pseudo code:
+    Constraints:
+        3<=N<=10^7
+        1<=Q<=2*10^5
+        1<=start_x<=end_x<=N
+        0<=cost<=10^9
+    Example:
+    5 3
+    1 2 100
+    2 5 100
+    3 4 100
+    take array size of 6.
+    arr[6]={0,0,0,0,0,0};
+    /*now remember only index from 1 to 2 must be increased */
+    instead of increasing every thing at a time we increase a[1]+=100 and decrease a[3]-=100
+    arr[6]={0,100,0,-100,0,0};
+    similarly
+    arr[2]+=100 arr[5+1] is more than MAX size so neglect it;
+    arr[6]={0,100,100,-100,0,0};
+    arr[3]+=100 arr[4+1]-=100
+    arr[6]={0,100,100,0,0,-100};
+    arr[i]=arr[i-1]+arr[i] PREFIX SUM
+    arr[6]={0,100,200,200,200,100};
+
+    REMBEMBER size here is more than i.e N is greater than 10^6 so use vector(dynamic allocation)
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
