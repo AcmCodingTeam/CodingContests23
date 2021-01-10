fcdcbb6ce203c085aec49c8963485fd812974973
karthik224-hash <68278634+karthik224-hash@users.noreply.github.com>
2021-01-10 16:14:08 +0530

    Update santadigit.cpp

--- Contest 2/santadigit.cpp
+++ Contest 2/santadigit.cpp
@@ -1,3 +1,28 @@
+/*
+link:https://www.hackerrank.com/contests/junior-challenge2/challenges/santa-digit/problem
+    pseudo code:
+    constriants:
+    1<=N<10^100000 (10^5+1) digits
+    1<=K<=10^5
+   ll digit_sum(ll n){
+            if(l<=9)
+                return l;
+            /*code on  sum of digits */
+            digit_sum(l);   
+    }
+    main(){
+        as N is grater than 10^18 it will not fit into long long datatype so take string
+        find sum of it 
+         string s;   
+         s[i]-'0'   
+         ascii value of present character-ascii value of 0
+         ascii of 0 is 48.
+         if present character value may be from (0-9) i.e(48-57)    
+         call digit_sum function   
+    }    
+
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
